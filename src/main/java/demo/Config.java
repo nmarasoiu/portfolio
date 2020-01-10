91d95baacac471c97366214cfb234dfcec820d4b
nicu <nicolae.marasoiu@gmail.com>
2020-01-10 13:57:30 +0200

    order fields

--- src/main/java/demo/Config.java
+++ src/main/java/demo/Config.java
@@ -4,8 +4,7 @@ import java.time.Duration;
 
 //todo from properties?
 class Config {
-    static final int MIN_JSON_LEN_FOR_ERR = 21;
-    static final Duration HTTP_REQ_TIMEOUT = Duration.ofSeconds(30);
-
     static final String URL_FMT = "https://min-api.cryptocompare.com/data/price?fsym=%s&tsyms=EUR";//properties?
+    static final Duration HTTP_REQ_TIMEOUT = Duration.ofSeconds(30);
+    static final int MIN_JSON_LEN_FOR_ERR = 21;
 }
