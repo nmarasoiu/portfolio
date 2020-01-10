9c33c9a5965fc86095541488894b7cd8f510e287
nicu <nicolae.marasoiu@gmail.com>
2020-01-10 13:56:58 +0200

    todo

--- src/main/java/demo/Config.java
+++ src/main/java/demo/Config.java
@@ -2,6 +2,7 @@ package demo;
 
 import java.time.Duration;
 
+//todo from properties?
 class Config {
     static final int MIN_JSON_LEN_FOR_ERR = 21;
     static final Duration HTTP_REQ_TIMEOUT = Duration.ofSeconds(30);
